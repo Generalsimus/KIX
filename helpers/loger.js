30a065c9b5be6c972199b1b89aac894a0cadc3c6
Generalsimus <totorotomoney@gmail.com>
2021-12-08 04:21:48 +0400

    restore

---
+++ helpers/loger.js
@@ -0,0 +1,47 @@
+import consola from "consola"
+import { CustomError } from "./customError";
+
+
+
+const savedLogs = [];
+const colors = {
+    white: "\x1b[37m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    red: "\x1b[31m",
+    blue: "\x1b[34m"
+};
+
+
+export const saveLog = (logObject) => {
+    for (const Logstring in logObject) {
+        savedLogs.push(`${colors[logObject[Logstring]] + Logstring}\x1b[0m`)
+    }
+    console.clear();
+    console.log.apply(null, savedLogs)
+}
+
+export const log = (logObject) => {
+    const logs = [...savedLogs]
+    for (const Logstring in logObject) {
+        logs.push(`${colors[logObject[Logstring]] + Logstring}\x1b[0m`)
+    }
+    console.clear();
+    console.log.apply(null, logs)
+}
+
+export const clareLog = (logObject) => {
+    const logs = []
+    for (const Logstring in logObject) {
+        logs.push(`${colors[logObject[Logstring]] + Logstring}\x1b[0m`)
+    }
+    console.clear();
+    console.log.apply(null, logs)
+}
+
+
+
+
+export const logError = (error) => {
+    consola.error(new CustomError(error))
+}
